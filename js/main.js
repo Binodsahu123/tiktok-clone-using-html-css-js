b2ec831ec28842043692703b5318c6f5f7f5e6b1
mhmd2991 <mohamad.salimi@hotmail.com>
2023-03-23 20:20:56 +0200

    upload new js code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-let menuLis = document.querySelectorAll('.menu-bar ul li'),
-    sections = document.querySelectorAll('section.box');
+let menuLis = document.querySelectorAll('.menu-bar ul li:not(.notactive)'),
+    sections = document.querySelectorAll('section.box'),
+    homeSection = document.querySelector('.home'),
+    homeVideo = document.querySelector('.home video'),
+    friendsVideo = document.querySelector('.friends video');
 
 menuLis.forEach((li) => {
     li.addEventListener('click', (e) => {
@@ -11,5 +14,9 @@ menuLis.forEach((li) => {
             section.classList.remove('active');
         });
         document.querySelector('.' + e.currentTarget.dataset.sec).classList.add('active');
+        e.currentTarget.dataset.sec === "home" ? homeVideo.play() : homeVideo.pause();
+        e.currentTarget.dataset.sec === "friends" ? friendsVideo.play() : friendsVideo.pause();
     });
 });
+
+homeSection.classList.contains('active') ?  homeVideo.play() : homeVideo.pause();
